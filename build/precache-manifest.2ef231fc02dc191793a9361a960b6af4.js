50bb9db8ab827ed97f01d73e0a3ffa092e6b6150
Liubomyr Kokor <ljubik@users.noreply.github.com>
2019-01-05 12:11:40 +0200

    total comparsion with local

---
+++ build/precache-manifest.2ef231fc02dc191793a9361a960b6af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73af04928ace5b2e329c",
+    "url": "/static/js/main.73af0492.chunk.js"
+  },
+  {
+    "revision": "cd17b7c69bd40d17eb12",
+    "url": "/static/js/1.cd17b7c6.chunk.js"
+  },
+  {
+    "revision": "73af04928ace5b2e329c",
+    "url": "/static/css/main.82c4a87e.chunk.css"
+  },
+  {
+    "revision": "cd17b7c69bd40d17eb12",
+    "url": "/static/css/1.25d8d205.chunk.css"
+  },
+  {
+    "revision": "28405de7db1c6237db55c50c23f1effa",
+    "url": "/index.html"
+  }
+];
